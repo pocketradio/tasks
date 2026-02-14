1cececbeedb4a5ad0bafacc36d0d93e4243348d4
cloudwolf <206946888+xerxes324@users.noreply.github.com.>
2026-02-14 10:51:08 +0530

    add basic shell functionality

--- cmd/add.go
+++ cmd/add.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -10,7 +11,8 @@ var addCmd = &cobra.Command{
 	Use:   "add",
 	Short: "Adds a task to your list.",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("add is called...")
+		task := strings.Join(args, " ")
+		fmt.Println("added task : ", task)
 	},
 }
 
--- cmd/do.go
+++ cmd/do.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/spf13/cobra"
 )
@@ -10,7 +11,18 @@ var doCmd = &cobra.Command{
 	Use:   "do",
 	Short: "Marks a task as complete",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("do called")
+
+		var ids []int
+		for _, arg := range args {
+			id, err := strconv.Atoi(arg) // akin to parseint
+			if err != nil {
+				fmt.Println("failed to parse the arg: ", arg)
+			} else {
+				ids = append(ids, id)
+			}
+		}
+
+		fmt.Println(ids) // prints out all IDS after checking if they're numbers.
 	},
 }
 
