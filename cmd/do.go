3ccafbcb6e0e8d23953426da6710c4fbf6b18bf0
cloudwolf <206946888+xerxes324@users.noreply.github.com.>
2026-02-14 20:37:10 +0530

    update do cmd

--- cmd/do.go
+++ cmd/do.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"strconv"
+	"task/db"
 
 	"github.com/spf13/cobra"
 )
@@ -22,7 +23,27 @@ var doCmd = &cobra.Command{
 			}
 		}
 
-		fmt.Println(ids) // prints out all IDS after checking if they're numbers.
+		tasks, err := db.AllTasks()
+		if err != nil {
+			fmt.Println("something went wrong :(", err)
+			return
+		}
+
+		for _, id := range ids {
+			if id <= 0 || id > len(tasks) {
+				fmt.Println("\nenter a valid task number.\n")
+				continue // cuz other task numbers might be valid ( assuming user wants to delete multiple tasks at the same time)
+			}
+
+			task := tasks[id-1]
+			err := db.DeleteTask(task.Key)
+			if err != nil {
+				fmt.Printf("failed to mark task '%d' as completed. Error : %s\n", id, err)
+			} else {
+				fmt.Printf("\nmarked task \"%d\" as completed\n", id)
+			}
+		}
+		// fmt.Println(ids) // prints out all IDS after checking if they're numbers.
 	},
 }
 
